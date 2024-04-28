c46544b3b3f33a22c3a758a2d3524c608e88286f
Armando <armandonavarro1024@gmail.com>
2024-04-28 15:44:26 -0500

    Quartz sync: Apr 28, 2024, 3:44 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,13 +18,13 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-	Component.Footer({
-		links: {
-		"Beastiary": "https://abbeyroad7.github.io/Terra-CoTs/World/Beastiary/",
-		"Dictionary": "https://abbeyroad7.github.io/Terra-CoTs/World/Dictionary/",
-		"History": "https://abbeyroad7.github.io/Terra-CoTs/World/History/",
-		},
-	}),
+	//Component.Footer({
+	//	links: {
+	//	"Beastiary": "https://abbeyroad7.github.io/Terra-CoTs/World/Beastiary/",
+	//	"Dictionary": "https://abbeyroad7.github.io/Terra-CoTs/World/Dictionary/",
+	//	"History": "https://abbeyroad7.github.io/Terra-CoTs/World/History/",
+	//	},
+	//}),
     Component.Breadcrumbs(),
     Component.ContentMeta(),
     Component.TagList(),
