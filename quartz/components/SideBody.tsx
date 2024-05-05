5a51a61b42b8e2fda7735ac0688a84c33cfe984a
Armando <armandonavarro1024@gmail.com>
2024-05-04 21:15:15 -0500

    Quartz sync: May 4, 2024, 9:15 PM

--- quartz/components/SideBody.tsx
+++ quartz/components/SideBody.tsx
@@ -1,19 +1,19 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
-const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
+const SideBody: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   const title = fileData.frontmatter?.title
   if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+    return <h1 class={classNames(displayClass, "side-body")}>{title}</h1>
   } else {
     return null
   }
 }
 
-ArticleTitle.css = `
+SideBody.css = `
 .article-title {
   margin-bottom: 2rem;
 }
 `
 
-export default (() => ArticleTitle) satisfies QuartzComponentConstructor
+export default (() => SideBody) satisfies QuartzComponentConstructor
