5638cf6b1ea3b8141a7780289e417d38a3448cdd
Armando <armandonavarro1024@gmail.com>
2024-05-06 22:15:08 -0500

    Quartz sync: May 6, 2024, 10:15 PM

--- quartz/components/SideBody.tsx
+++ quartz/components/SideBody.tsx
@@ -3,16 +3,13 @@ import { classNames } from "../util/lang"
 import { joinSegments } from "../util/path"
 import { pathToRoot } from "../util/path"
 
+
 const SideBody: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const title = fileData.frontmatter?.title
-  //const baseDir = pathToRoot(fileData.slug!)
   const baseDir = pathToRoot(fileData.slug!)
-  //base const description = joinSegments(pathToRoot, "static/DesktopLogo.jpg")
   const sidebody = fileData.frontmatter?.sidebody
   const sidebody2 = fileData.frontmatter?.sidebody2
   const sidebody3 = fileData.frontmatter?.sidebody3
-  //test const description = joinSegments(baseDir, "/content/test.md")
-  if (title) {
+  if (sidebody) {
     return <h1 class={classNames(displayClass, "side-body")}>	<font size="3">{sidebody}	<br><br>{sidebody2}</br></br>	<br><br>{sidebody3}</br></br>		</font></h1>
   } else {
     return null
