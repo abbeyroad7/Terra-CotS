d0085b2fd6d2bae4185627cc2144262f69b67971
Armando <armandonavarro1024@gmail.com>
2024-05-11 23:26:49 -0500

    Quartz sync: May 11, 2024, 11:26 PM

--- quartz/components/SideFooter.tsx
+++ quartz/components/SideFooter.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     const linksSide = opts?.linksSide ?? []
     return (
       <sidefooter class={classNames(displayClass, "side-footer")}>
-		<div style="padding:15px;"><iframe frameBorder="0" width="400" height="400" src="https://bestvibes.me/quote-of-the-day"></iframe></div>
+		<div style="padding:5px;"><iframe frameBorder="0" width="300" height="300" src="https://bestvibes.me/quote-of-the-day"></iframe></div>
 		<h3>Next Session: Tuesday, May 14 @6:30pm CT</h3>
 		<br></br>
 		
