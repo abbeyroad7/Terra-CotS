df88c3cc4eb712fa5d1c837361c61fe7f035ac56
Armando <armandonavarro1024@gmail.com>
2024-05-13 19:01:50 -0500

    Quartz sync: May 13, 2024, 7:01 PM

--- quartz/components/SideBody.tsx
+++ quartz/components/SideBody.tsx
@@ -10,7 +10,7 @@ const SideBody: QuartzComponent = ({ fileData, displayClass }: QuartzComponentPr
   
   const home = fileData.frontmatter?.home
   if (home) {
-	   return <div id='QOTD'><iframe scrolling="no" frameborder="yes" clocktype="html5" style="overflow:hidden;border:2;margin:10;padding:0;width:300px;height:300px;" src="https://bestvibes.me/quote-of-the-day"></iframe></div>
+	   return <div id='QOTD'><iframe scrolling="no" frameborder="no" clocktype="html5" style="overflow:hidden;border:2;margin:10;padding:0;width:300px;height:300px;" src="https://bestvibes.me/quote-of-the-day"></iframe></div>
 
   } else {
 	  return null
