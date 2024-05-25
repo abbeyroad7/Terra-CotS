32b1ffcac6ebca2983db5c05b58da52882a6b290
Armando <armandonavarro1024@gmail.com>
2024-05-24 22:05:26 -0500

    Quartz sync: May 24, 2024, 10:05 PM

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -2,12 +2,19 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import { classNames } from "../util/lang"
 
 const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const title = fileData.frontmatter?.title
-  if (title) {
-    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+  const landing = fileData.frontmatter?.landing
+  if (landing) {
+	   return null
   } else {
-    return null
+	  const title = fileData.frontmatter?.title
+		if (title) {
+			return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+		} else {
+			return null
+		}
   }
+	
+  
 }
 
 ArticleTitle.css = `
