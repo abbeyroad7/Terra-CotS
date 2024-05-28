c6c42a73b6b3a6497c9f074385af8d80aef03fa8
abbeyroad7 <64578026+abbeyroad7@users.noreply.github.com>
2024-05-28 08:59:11 -0500

    Update SideFooter.tsx

--- quartz/components/SideFooter.tsx
+++ quartz/components/SideFooter.tsx
@@ -17,14 +17,14 @@ export default ((opts?: Options) => {
 		<h3>Next Session: Tuesday, May 28 @6:30pm CT</h3>
 
 		<h4 style="text-align: left; width:49%; display: inline-block;">Navigation</h4>
-		<h4 style="text-align: right; width:50%;  display: inline-block;">v0.44.073</h4>
+		<h4 style="text-align: right; width:50%;  display: inline-block;">v0.44.074</h4>
 		
           {Object.entries(linksSide).map(([text, linksSide]) => (
               <br><a href={linksSide}>{text}</a></br>
           ))}
-	<br><br>
+	<br><br><br><br>
 		<div style="text-align: right; width:100%;  display: inline-block; font-size: 14px;">Due to an issue in Quartz, you will need to zoom out in your browser until both sidebars are visible.</div>
-	</br></br>
+	</br></br></br></br>
       </sidefooter>
     )
   }
@@ -33,7 +33,7 @@ SideFooter.css = `
 .side-footer {
   margin-top: -2rem;
   margin-left: -8rem;
-  line-height: 55%;
+  line-height: 60%;
   
   h3 {
 	  line-height: 130%;
